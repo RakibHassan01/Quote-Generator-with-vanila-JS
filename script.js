8c364df935807087cd575658b3583145e4765acc
Limitless-Rakib <80609225+Rakib-Coder00@users.noreply.github.com>
2021-11-07 08:59:43 +0600

    manipulating DOM

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+
+
 let apiQuotes = []
 
 // show new quote  
 function newQuote() {
     // Pick random quote from array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-    console.log(quote);
+    authorText.textContent = quote.author
+    quoteText.textContent = quote.text
 }
 
 // Get Quote From API 
@@ -24,3 +33,13 @@ async function getQuotes() {
 // On Load 
 getQuotes()
 
+
+// Tweet a quote 
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+    window.open(twitterUrl, '_blank')
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
