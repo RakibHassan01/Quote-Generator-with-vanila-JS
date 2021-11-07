ea109e6691bddd33cf3924f03784c26936396b06
Limitless-Rakib <80609225+Rakib-Coder00@users.noreply.github.com>
2021-11-07 09:13:31 +0600

    created loader

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
 
 
 
@@ -10,14 +12,18 @@ let apiQuotes = []
 
 // show new quote  
 function newQuote() {
+    loading()
     // Pick random quote from array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
     authorText.textContent = quote.author
+
     quoteText.textContent = quote.text
+    complete()
 }
 
 // Get Quote From API 
 async function getQuotes() {
+    loading()
     const apiUrl = 'https://type.fit/api/quotes'
     try {
         const response = await fetch(apiUrl)
@@ -43,3 +49,16 @@ function tweetQuote() {
 // Event listeners
 newQuoteBtn.addEventListener('click', newQuote)
 twitterBtn.addEventListener('click', tweetQuote)
+
+
+// Show loading
+function loading() {
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+// Hide Loading 
+function complete() {
+    loader.hidden = true
+    quoteContainer.hidden = false
+}
